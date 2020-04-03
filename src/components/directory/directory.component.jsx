5282dd3ba366c9cc6497afea62ab6416812a1dca
unknown <mcvilleanrapsody@gmail.com>
2020-04-04 01:35:10 +0800

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,7 +10,7 @@ export class Directory extends Component {
             sections: [
                 {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: 'https://images.pexels.com/photos/1124465/pexels-photo-1124465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                     id: 1,
                     linkUrl: 'hats'
                 },
@@ -28,14 +28,14 @@ export class Directory extends Component {
                 },
                 {
                     title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: 'https://images.unsplash.com/photo-1584270413639-d5ee397272cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                 },
                 {
                     title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imageUrl: 'https://images.unsplash.com/photo-1512236393941-3d6da97e56bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
                     size: 'large',
                     id: 5,
                     linkUrl: 'shop/mens'
