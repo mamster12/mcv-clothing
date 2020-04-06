56b44b19ed1bcd4170c967b481afec6f10bdac46
unknown <mcvilleanrapsody@gmail.com>
2020-04-07 02:04:54 +0800

    move directory, and shop date into it's own reducer, create respective selector and update corresponding components wit new redux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,61 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
 import MenuItem from '../menu-item/menu-item.components';
 import './directory.styles.scss';
 
-export class Directory extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://images.pexels.com/photos/1124465/pexels-photo-1124465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    id: 1,
-                    linkUrl: 'hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: 'shop/jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: 'shop/sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://images.unsplash.com/photo-1584270413639-d5ee397272cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: 'shop/womens'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://images.unsplash.com/photo-1512236393941-3d6da97e56bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'shop/mens'
-                }
-            ]
-        }
-
-    };
-
-    render() {
-        return (
-            <div className="directory-menu">
-                {this.state.sections.map(({ id, ...otherSectionProps }) => (
-                    <MenuItem key={id} {...otherSectionProps} />
-                )
-                )}
-            </div>
-        )
-    }
+const Directory = ({ sections }) => {
+    return (
+        <div className="directory-menu">
+            {sections.map(({ id, ...otherSectionProps }) => (
+                <MenuItem key={id} {...otherSectionProps} />
+            )
+            )}
+        </div>
+    )
 }
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
 
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,45 @@
+const INITIAL_STATE = {
+    sections: [
+        {
+            title: 'hats',
+            imageUrl: 'https://images.pexels.com/photos/1124465/pexels-photo-1124465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            id: 1,
+            linkUrl: 'shop/hats'
+        },
+        {
+            title: 'jackets',
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            id: 2,
+            linkUrl: 'shop/jackets'
+        },
+        {
+            title: 'sneakers',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            id: 3,
+            linkUrl: 'shop/sneakers'
+        },
+        {
+            title: 'womens',
+            imageUrl: 'https://images.unsplash.com/photo-1584270413639-d5ee397272cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+            size: 'large',
+            id: 4,
+            linkUrl: 'shop/womens'
+        },
+        {
+            title: 'mens',
+            imageUrl: 'https://images.unsplash.com/photo-1512236393941-3d6da97e56bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
+            size: 'large',
+            id: 5,
+            linkUrl: 'shop/mens'
+        }
+    ]
+};
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default directoryReducer;
